31dfa4661ce754ba4ab372701de84037b0e3cc33
Yura <linetsky.yura@gmail.com>
2023-02-13 09:16:19 +0200

    heart

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Happy Valentine myLittleGirl</title>
+        <script src="https://kit.fontawesome.com/699e6f5ea8.js" crossorigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,22 @@
+import styles from '@/styles/Home.module.sass'
+import clsx from 'clsx'
+import { useState } from 'react'
+
+export const Home = () => {
+  const [tap, setTap] = useState(true)
+  return (
+    <>
+      <div className={clsx(styles.modal_holder, tap ? styles.show_holder : '')}></div>
+      <div className={clsx(styles.modal_window, tap ? styles.show_window : '')}>
+        <div className={styles.wrapper}>
+          <div className={styles.heart_block}>
+            <div onClick={() => setTap(false)} className={styles.heart}></div>
+            <h1>Tap Tap!</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
